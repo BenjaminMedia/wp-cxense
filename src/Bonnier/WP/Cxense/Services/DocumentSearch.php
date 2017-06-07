80f39e119cc4f121c24e426ca614fe0779613360
Ovidiu Ostafi <ovidiu.ostafi@webreference.ro>
2017-06-07 15:06:02 +0100

    mergin facets into search call and adding highlights and spellcheck

--- src/Bonnier/WP/Cxense/Services/DocumentSearch.php
+++ src/Bonnier/WP/Cxense/Services/DocumentSearch.php
@@ -7,12 +7,11 @@ namespace Bonnier\WP\Cxense\Services;
 
 use Bonnier\WP\Cxense\Exceptions\DocumentSearchMissingSearch;
 use Bonnier\WP\Cxense\Exceptions\DocumentSearchMissingCount;
-use Bonnier\WP\Cxense\Exceptions\DocumentSearchMissingFacet;
+use Bonnier\WP\Cxense\Exceptions\DocumentSearchWrongFacet;
 use Bonnier\WP\Cxense\Exceptions\DocumentSearchWrongFilter;
 use Bonnier\WP\Cxense\Http\HttpRequest;
 use Bonnier\WP\Cxense\Settings\SettingsPage;
 use Bonnier\WP\Cxense\Parsers\Document;
-use Bonnier\WP\Cxense\Parsers\Facet;
 
 /**
  * DocumentSearch class
@@ -81,11 +80,11 @@ class DocumentSearch {
 		//
 	}
 
-    /**
-     * Singleton implementation
-     *
-     * @return DocumentSearch
-     */
+	/**
+	 * Singleton implementation
+	 *
+	 * @return DocumentSearch
+	 */
 	public static function get_instance() {
 		if (!isset(self::$objInstance)) {
 			$obj = __CLASS__;
@@ -111,11 +110,17 @@ class DocumentSearch {
 		return $this;
 	}
 
-	public function set_search($arrSearch) {
-	    $this->arrSearch = $arrSearch;
+	/**
+	 * Set search input array
+	 *
+	 * @param array $arrSearch
+	 * @return DocumentSearch
+	 */
+	public function set_search(array $arrSearch) {
+		$this->arrSearch = $arrSearch;
 
-	    return $this;
-    }
+		return $this;
+	}
 	
 	/**
 	 * Get documents from search result
@@ -124,40 +129,20 @@ class DocumentSearch {
 	 */
 	public function get_documents() {
 
-        $this->validate_query_key();
+		$this->validate_query_key();
 		
-		$objDocuments = $this->set_per_page()->set_page()->set_filter()->set_result_fields()->get();
+		$objDocuments = $this
+			->set_per_page()
+			->set_page()
+			->set_facets()
+			->set_filter()
+			->set_spellcheck()
+			->set_highlights()
+			->set_result_fields()
+			->get();
 		
-		return [
-			'totalCount' => $objDocuments->totalCount,
-			'matches' => $this->parse_documents($objDocuments->matches)
-		];
-	}
-	
-	/**
-	 * Set the facets
-	 *
-	 *
-	 * @return DocumentSearch
-	 */
-	public function get_facets() {
-		
-		$this->validate_facet_key();
-		
-		$this->arrPayload['facets'][] = [
-			'type' => 'string',
-			'field' => $this->arrSearch['facet_field'],
-			'count' => 5,
-			'min' => 1
-		];
-		
-		$objDocuments = $this->set_per_page(0)->set_filter()->get();
-		
-		return [
-			'totalCount' => $objDocuments->totalCount,
-			'matches' => $this->parse_facets($objDocuments->facets[0]->buckets)
-		];
-		
+		$objDocuments->matches = $this->parse_documents($objDocuments->matches);
+		return $objDocuments;
 	}
 	
 	/**
@@ -178,27 +163,15 @@ class DocumentSearch {
 		return json_decode($objResponse->getBody());
 	}
 
-    /**
-     * Validate the search request
-     *
-     * @return null
-     * @throws DocumentSearchMissingSearch
-     */
+	/**
+	 * Validate the search request
+	 *
+	 * @return null
+	 * @throws DocumentSearchMissingSearch
+	 */
 	private function validate_query_key() {
 		if (!isset($this->arrSearch['query'])) {
 			throw new DocumentSearchMissingSearch('Missing request "query" key!');
-		}
-	}
-
-    /**
-     * Validate the facet request
-     *
-     * @return null
-     * @throws DocumentSearchMissingFacet
-     */
-	private function validate_facet_key() {
-		if (!isset($this->arrSearch['facet_field'])) {
-			throw new DocumentSearchMissingFacet('Missing request "facet_field" key!');
 		}
 	}
 	
@@ -222,13 +195,46 @@ class DocumentSearch {
 		$this->arrPayload['token-op'] = 'and';
 		return $this;
 	}
-
-    /**
-     * Set filter
-     *
-     * @return DocumentSearch
-     * @throws DocumentSearchWrongFilter
-     */
+	
+	/**
+	 * Set highlights
+	 *
+	 * @return DocumentSearch
+	 */
+	private function set_highlights() {
+		
+		if (isset($this->arrSearch['highlights'])) {
+			$this->arrPayload['highlights'] = $this->arrSearch['highlights'];
+		}
+		
+		return $this;
+	}
+	
+	/**
+	 * Set facets
+	 *
+	 * @return DocumentSearch
+	 * @throws DocumentSearchWrongFacet
+	 */
+	private function set_facets() {
+		
+		if (isset($this->arrSearch['facets'])) {
+			if (!is_array($this->arrSearch['facets'])) {
+				throw new DocumentSearchWrongFacet('"Facets" key is not an array');
+			}
+			
+			$this->arrPayload['facets'] = $this->arrSearch['facets'];
+		}
+		
+		return $this;
+	}
+	
+	/**
+	 * Set filter
+	 *
+	 * @return DocumentSearch
+	 * @throws DocumentSearchWrongFilter
+	 */
 	private function set_filter() {
 		
 		if (isset($this->arrSearch['filter'])) {
@@ -269,13 +275,13 @@ class DocumentSearch {
 		return $this;
 	}
 
-    /**
-     * Set page number for calculating the correct starting offset
-     *
-     * @param integer $intPage Page number
-     * @return Search
-     * @throws DocumentSearchMissingCount
-     */
+	/**
+	 * Set page number for calculating the correct starting offset
+	 *
+	 * @param integer $intPage Page number
+	 * @return Search
+	 * @throws DocumentSearchMissingCount
+	 */
 	private function set_page($intPage = 1) {
 		
 		if (!isset($this->arrPayload['count']) || !$this->arrPayload['count']) {
@@ -314,6 +320,19 @@ class DocumentSearch {
 	}
 	
 	/**
+	 * Set spellcheck
+	 *
+	 * @return DocumentSearch
+	 */
+	private function set_spellcheck() {
+		
+		if (isset($this->arrSearch['spellcheck']) && $this->arrSearch['spellcheck'] === true) {
+			$this->arrPayload['spellcheck']['query'] = $this->arrSearch['query'];
+		}
+		return $this;
+	}
+	
+	/**
 	 * Parse documents to cxense object
 	 *
 	 * @param array $arrDocuments
@@ -324,22 +343,6 @@ class DocumentSearch {
 		
 		foreach ($arrDocuments as $objDocument) {
 			$arrCollection[] = new Document($objDocument);
-		}
-		
-		return $arrCollection;
-	}
-	
-	/**
-	 * Parse facets to cxense object
-	 *
-	 * @param array $arrFacets
-	 * @return array
-	 */
-	private function parse_facets(array $arrFacets) {
-		$arrCollection = [];
-		
-		foreach ($arrFacets as $objFacet) {
-			$arrCollection[] = new Facet($objFacet);
 		}
 		
 		return $arrCollection;
