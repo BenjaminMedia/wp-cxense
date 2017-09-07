1f403851319fb3628c58d965c2e101fd6525c8d5
Lms <lms-pgm@hotmail.fr>
2017-09-07 11:30:52 +0000

    Apply fixes from StyleCI

--- src/Bonnier/WP/Cxense/Exceptions/DocumentSearchMissingSearch.php
+++ src/Bonnier/WP/Cxense/Exceptions/DocumentSearchMissingSearch.php
@@ -8,14 +8,16 @@ namespace Bonnier\WP\Cxense\Exceptions;
 /**
  * DocumentSearchMissingSearch class
  */
-class DocumentSearchMissingSearch extends \Exception {
-	
-	/**
-	 * Constructor
-	 *
-	 * @return DocumentSearchMissingSearch
-	 */
-	public function __construct($strMessage = '') {
-		return parent::__construct($strMessage, 10);
-	}
+class DocumentSearchMissingSearch extends \Exception
+{
+    
+    /**
+     * Constructor
+     *
+     * @return DocumentSearchMissingSearch
+     */
+    public function __construct($strMessage = '')
+    {
+        return parent::__construct($strMessage, 10);
+    }
 }
--- src/Bonnier/WP/Cxense/Services/CxenseApi.php
+++ src/Bonnier/WP/Cxense/Services/CxenseApi.php
@@ -10,50 +10,51 @@ use Exception;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
 
-class CxenseApi {
-
-	const EXCEPTION_USER_NOT_DEFINED = 0;
-	const EXCEPTION_UNAUTHORIZED = 1;
-	const EXCEPTION_TIME_OUT = 2;
-	const CXENSE_PROFILE_PUSH = '/profile/content/push';
-	const CXENSE_PROFILE_DELETE = '/profile/content/delete';
-	const CXENSE_WIDGET_DATA = '/public/widget/data';
+class CxenseApi
+{
+    const EXCEPTION_USER_NOT_DEFINED = 0;
+    const EXCEPTION_UNAUTHORIZED = 1;
+    const EXCEPTION_TIME_OUT = 2;
+    const CXENSE_PROFILE_PUSH = '/profile/content/push';
+    const CXENSE_PROFILE_DELETE = '/profile/content/delete';
+    const CXENSE_WIDGET_DATA = '/public/widget/data';
 
     const CACHE_BASE_URI = 'https://cache-service.bonnier.cloud';
     const CACHE_UPDATE = '/api/v1/update';
     const CACHE_DELETE = '/api/v1/delete';
 
 
-	/* @var SettingsPage $settings */
-	protected static $settings;
+    /* @var SettingsPage $settings */
+    protected static $settings;
 
     /**
      * @param SettingsPage $settingsPage
      */
-    public static function bootstrap(SettingsPage $settingsPage) {
-		self::$settings = $settingsPage;
-	}
+    public static function bootstrap(SettingsPage $settingsPage)
+    {
+        self::$settings = $settingsPage;
+    }
 
     /**
      * @param $widgetId
      * @return null
      */
-    public static function get_widget_data($widgetId) {
+    public static function get_widget_data($widgetId)
+    {
+        $objResponse = HttpRequest::get_instance()->post(self::CXENSE_WIDGET_DATA, [
+            'timeout' => 2,
+            'redirection' => 1,
+            'body' => json_encode([
+                'widgetId' => $widgetId
+            ])
+        ]);
 
-		$objResponse = HttpRequest::get_instance()->post(self::CXENSE_WIDGET_DATA, [
-			'timeout' => 2,
-			'redirection' => 1,
-			'body' => json_encode([
-				'widgetId' => $widgetId
-			])
-		]);
+        if ($objResponse->getStatusCode() !== 200) {
+            return null;
+        };
 
-		if ($objResponse->getStatusCode() !== 200) {
-			return null;
-		};
-
-		return json_decode($objResponse->getBody())->items;
-	}
+        return json_decode($objResponse->getBody())->items;
+    }
 
 
     /**
@@ -61,57 +62,53 @@ class CxenseApi {
      * @param bool $delete
      * @return bool|null
      */
-    public static function pingCrawler($postId, $delete = false) {
-
-		if( !wp_is_post_revision($postId) && !wp_is_post_autosave($postId) ) {
-
-			$contentUrl = is_numeric($postId) ? get_permalink($postId) : $postId;
+    public static function pingCrawler($postId, $delete = false)
+    {
+        if (!wp_is_post_revision($postId) && !wp_is_post_autosave($postId)) {
+            $contentUrl = is_numeric($postId) ? get_permalink($postId) : $postId;
 
             $apiPath = $delete || !Post::is_published($postId) ? self::CACHE_DELETE : self::CACHE_UPDATE;
 
-			try {
+            try {
                 return self::CacheService($apiPath, $contentUrl);
-            } catch(Exception $e) {
+            } catch (Exception $e) {
+                if ($e instanceof HttpException) {
+                    error_log('WP cXense: Failed calling cXense api: ' . $apiPath . ' response code: '. $e->getCode() .' error: ' . $e->getMessage());
+                }
 
-				if($e instanceof HttpException) {
-					error_log('WP cXense: Failed calling cXense api: ' . $apiPath . ' response code: '. $e->getCode() .' error: ' . $e->getMessage());
-				}
+                if ($e->getCode() == self::EXCEPTION_USER_NOT_DEFINED) {
+                    error_log('PHP Warning: To use CXense push you must define constants CXENSE_USER_NAME and CXENSE_API_KEY');
+                } elseif ($e->getCode() == self::EXCEPTION_UNAUTHORIZED) {
+                    error_log('PHP Warning: Could not authorize with defined CXENSE_USER_NAME and CXENSE_API_KEY');
+                }
+            }
+        }
+        return null;
+    }
 
-				if( $e->getCode() == self::EXCEPTION_USER_NOT_DEFINED ) {
-					error_log('PHP Warning: To use CXense push you must define constants CXENSE_USER_NAME and CXENSE_API_KEY');
-				} elseif( $e->getCode() == self::EXCEPTION_UNAUTHORIZED ) {
-					error_log('PHP Warning: Could not authorize with defined CXENSE_USER_NAME and CXENSE_API_KEY');
-				}
-			}
-		}
-		return null;
-	}
+    /**
+     * @param string $path
+     * @param array|string $args
+     * @param int $timeout Seconds until timeout
+     * @return boolean
+     * @throws Exception
+     */
+    public static function request($path, $args, $timeout=5)
+    {
+        self::getCxenseUser();
 
-	/**
-	 * @param string $path
-	 * @param array|string $args
-	 * @param int $timeout Seconds until timeout
-	 * @return boolean
-	 * @throws Exception
-	 */
-	public static function request($path, $args, $timeout=5) {
-	    self::getCxenseUser();
+        $objResponse = HttpRequest::get_instance()->set_auth(self::$settings)->post($path, [
+            'body' => json_encode($args),
+            'timeout' => $timeout
+        ]);
 
-		$objResponse = HttpRequest::get_instance()->set_auth(self::$settings)->post($path, [
-			'body' => json_encode($args),
-			'timeout' => $timeout
-		]);
-
-		if( $objResponse->getStatusCode() === 401 ){
-
-			throw new Exception('Authorization required', self::EXCEPTION_UNAUTHORIZED);
-
-		} elseif( $objResponse->getStatusCode() < 200 || $objResponse->getStatusCode() >= 300 ) {
-
-			throw new Exception('Unexpected response, code: '.$objResponse->getStatusCode().' message: '.$objResponse->getMessage(), -1);
-		}
-		return true;
-	}
+        if ($objResponse->getStatusCode() === 401) {
+            throw new Exception('Authorization required', self::EXCEPTION_UNAUTHORIZED);
+        } elseif ($objResponse->getStatusCode() < 200 || $objResponse->getStatusCode() >= 300) {
+            throw new Exception('Unexpected response, code: '.$objResponse->getStatusCode().' message: '.$objResponse->getMessage(), -1);
+        }
+        return true;
+    }
 
     /**
      * @param $uri
@@ -128,11 +125,11 @@ class CxenseApi {
 
         try {
             $response = $client->post($uri, ['json' => ['url' => $url]]);
-        } catch(ClientException $e) {
+        } catch (ClientException $e) {
             return false;
         }
 
-        if(200 === $response->getStatusCode()) {
+        if (200 === $response->getStatusCode()) {
             $result = \json_decode($response->getBody());
             return isset($result->status) && 200 == $result->status;
         }
@@ -147,9 +144,8 @@ class CxenseApi {
     {
         $cxUser = self::$settings->get_api_user();
 
-        if( !$cxUser ) {
+        if (!$cxUser) {
             throw new Exception('You must define constants CXENSE_USER_NAME and CXENSE_API_KEY', self::EXCEPTION_USER_NOT_DEFINED);
         }
     }
-
 }
