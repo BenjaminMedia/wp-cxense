4381f8ebe984b83b00dababcae136deeea341a71
Alf Henderson <alfhenderson@gmail.com>
2017-11-20 12:23:04 +0100

    Added setting to change order of searchable taxonomies

--- src/Bonnier/WP/Cxense/Settings/Partials/CustomTaxonomiesSettings.php
+++ src/Bonnier/WP/Cxense/Settings/Partials/CustomTaxonomiesSettings.php
@@ -5,6 +5,7 @@ namespace Bonnier\WP\Cxense\Settings\Partials;
 class CustomTaxonomiesSettings
 {
     const SETTING_KEY = 'custom_taxonomies';
+    const SETTING_KEY_ORDER = 'custom_taxonomies_order';
     const DISABLED_TAXONOMIES = [
         'post_tag',
         'nav_menu',
@@ -18,22 +19,32 @@ class CustomTaxonomiesSettings
 
     public static function render($fieldName, $fieldValues)
     {
-        $taxonomies = get_taxonomies();
-        $output = "";
-
-        foreach ($taxonomies as $taxonomy) {
+        foreach (static::get_enabled_taxonomies() as $taxonomy) {
             $checked = isset($fieldValues[$taxonomy]) && $fieldValues[$taxonomy] == 1 ?  'checked' : '';
-            if (!in_array($taxonomy, static::DISABLED_TAXONOMIES)) {
-                $output .= "
-                    <strong>$taxonomy:</strong> 
-                    <input type='hidden' value='0' name='".$fieldName."[$taxonomy]'>
-                    <input type='checkbox' value='1' name='".$fieldName."[$taxonomy]' $checked />
-                    <br>
-                ";
-            }
+            echo "
+                <strong>$taxonomy:</strong> 
+                <input type='hidden' value='0' name='${fieldName}[$taxonomy]'>
+                <input type='checkbox' value='1' name='{$fieldName}[$taxonomy]' $checked />
+                <br>
+            ";
         }
+    }
 
-        echo $output;
+    public static function renderTaxonomyOrder($fieldName, $fieldValues)
+    {
+        $taxonomies = static::get_enabled_taxonomies();
+        $maxOrder = count($taxonomies);
+
+        echo "<p>Give each taxonomy a number from 1 and up to order the taxonomies, lower numbers come first</p>";
+
+        foreach ($taxonomies as $key => $taxonomy) {
+            $value = $fieldValues[$taxonomy] ?? ($key + 1);
+            echo "
+                <strong>$taxonomy:</strong> 
+                <input type='number' value='$value' min='1' max='$maxOrder' name='${fieldName}[$taxonomy]'>
+                <br>
+            ";
+        }
     }
 
     public static function sanitize_input($inputValues)
@@ -49,15 +60,13 @@ class CustomTaxonomiesSettings
         return $sanitizedInput;
     }
 
-    public static function get_printable_taxonomies()
-    {
-        $taxonomies = get_taxonomies();
-        $prinableTaxonomies = [];
-        foreach ($taxonomies as $taxonomy) {
-            if (!in_array($taxonomy, static::DISABLED_TAXONOMIES) && $taxonomy !== 'category') {
-                $prinableTaxonomies[] = $taxonomy;
-            }
-        }
-        return $prinableTaxonomies;
+    public static function get_printable_taxonomies() {
+        $taxonomies = static::get_enabled_taxonomies();
+        unset($taxonomies['category']); // Disable category as it is by default always printed
+        return $taxonomies;
+    }
+
+    private static function get_enabled_taxonomies() {
+        return array_diff(get_taxonomies(), static::DISABLED_TAXONOMIES);
     }
 }
