d50c02b7181dba7118814cb8a17b4d1fde4b0a25
Mohamed Salem Lamiri <lms-pgm@hotmail.fr>
2018-08-17 13:20:42 +0200

    Adding testconfig

---
+++ tests/test-config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_NAME', getenv('DB_NAME'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
+define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
+define('DB_CHARSET', 'utf8mb4');
+define('DB_COLLATE', '');
